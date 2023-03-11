6d6b094b68817804d2731de81d7664736a61ed68
Thai Bithope <thaibithope@gmail.com>
2023-03-11 10:27:00 +0000

    commit

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET blogs listing. */
 router.get("/", function (req, res, next) {
-    res.render("blogs/index", { data:"hello Mongoose" });
+    res.render("blogs/index", { data:"ข้อมูลบทความ" });
+});
+
+/* GET blogs/add listing. */
+router.get("/add", function (req, res, next) {
+    res.render("blogs/addForm", { data:"เพิ่มบทความ" });
 });
 
 module.exports = router;
